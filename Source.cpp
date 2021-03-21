405c3c668269ee830bbf1ea900115712d498e797
Olexandr_Z <zeeelensky@gmail.com>
2021-03-21 22:19:50 +0200

    edited vector

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,11 @@
 #include "Header.h"
+#include "Vector.h"
+
 using namespace std;
+
 int main() {
 	vector<string> matr = f_processing("inp.txt");
-	vector<vertice> vertices = get_vertices(matr);
+	std::vector<vertice> vertices = get_vertices(matr);
 
 	/* Zeeelensky, there is some place for your code) */
 
--- Vector.h
+++ Vector.h
@@ -1,16 +1,19 @@
 #pragma once
 
-#include <algorithm>
+#include <iostream>
 
 template <typename T>
-class vektor {
+class veñtor {
 public:
-    vektor() = default;
-    explicit vektor(size_t size);
-    vektor(const vektor& another);
-    ~vektor();
+    veñtor() = default;
+    explicit veñtor(size_t size);
+    veñtor(const veñtor& another);
+    ~veñtor();
 
     T& operator[](size_t index);
+
+    T* begin(void);
+    T* end(void);
 
     size_t size(void) const;
     size_t capacity(void) const;
@@ -20,8 +23,11 @@ public:
     void resize(size_t new_size);
     void resize(size_t new_size, T value);
 
-    void clear(void);
+    void reserve(size_t to_reserve);
 
+    void pop_back(void);
+
+    void clear(void);
 private:
     T* data = nullptr;
     size_t sz = 0;
@@ -31,41 +37,41 @@ private:
 };
 
 template <typename T>
-inline vektor<T>::vektor(size_t size) : data(new T[size]), sz(size), cp(size) {}
+inline veñtor<T>::veñtor(size_t size) : data(new T[size]), sz(size), cp(size) {}
 
 template <typename T>
-inline vektor<T>::vektor(const vektor& another) {
+inline veñtor<T>::veñtor(const veñtor& another) {
     memcpy(this, &another, sizeof(another));
 }
 
 template <typename T>
-inline vektor<T>::~vektor() {
+inline veñtor<T>::~veñtor() {
     delete[] data;
 }
 
 template <typename T>
-inline T& vektor<T>::operator[](size_t index) {
+inline T& veñtor<T>::operator[](size_t index) {
     return data[index];
 }
 
 template <typename T>
-inline size_t vektor<T>::size(void) const {
+inline size_t veñtor<T>::size(void) const {
     return sz;
 }
 
 template <typename T>
-inline size_t vektor<T>::capacity(void) const {
+inline size_t veñtor<T>::capacity(void) const {
     return cp;
 }
 
 template <typename T>
-inline void vektor<T>::push_back(const T& value) {
+inline void veñtor<T>::push_back(const T& value) {
     expand_check();
     data[sz++] = value;
 }
 
 template <typename T>
-inline void vektor<T>::expand_check(void) {
+inline void veñtor<T>::expand_check(void) {
     if (sz >= cp) {
         auto new_cap = cp == 0 ? 1 : 2 * cp;
         auto new_data = new T[new_cap];
@@ -76,15 +82,25 @@ inline void vektor<T>::expand_check(void) {
     }
 }
 
+template <typename T>
+inline T* veñtor<T>::begin(void) {
+    return data;
+}
 
 template <typename T>
-inline void vektor<T>::clear(void) {
+inline T* veñtor<T>::end(void) {
+    return data + sz;
+}
+
+
+template <typename T>
+inline void veñtor<T>::clear(void) {
     sz = 0;
 }
 
 
 template <typename T>
-inline void vektor<T>::resize(size_t to_resize) {
+inline void veñtor<T>::resize(size_t to_resize) {
     data = (T*)realloc(data, to_resize * sizeof(T));
     if (cp < to_resize) {
         data = (T*)realloc(data, to_resize * sizeof(T));
@@ -94,7 +110,7 @@ inline void vektor<T>::resize(size_t to_resize) {
 }
 
 template <typename T>
-inline void vektor<T>::resize(size_t to_resize, T value) {
+inline void veñtor<T>::resize(size_t to_resize, T value) {
     if (cp < to_resize) {
         data = (T*)realloc(data, to_resize * sizeof(T));
         cp = to_resize;
@@ -103,4 +119,17 @@ inline void vektor<T>::resize(size_t to_resize, T value) {
         for (size_t i = sz; i < to_resize; i++) data[i] = value;
     }
     sz = to_resize;
+}
+
+template <typename T>
+inline void veñtor<T>::reserve(size_t to_reserve) {
+    if (cp < to_reserve) {
+        data = (T*)realloc(data, to_reserve * sizeof(T));
+        cp = to_reserve;
+    }
+}
+
+template <typename T>
+inline void veñtor<T>::pop_back(void) {
+    sz--;
 }
