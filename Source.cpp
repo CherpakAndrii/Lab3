772b2b23d21844ac67e057070a324258fddb98d9
Andrew <andriycherpak.education@gmail.com>
2021-03-21 22:40:19 +0200

    removing "std::"

--- Source.cpp
+++ Source.cpp
@@ -1,13 +1,12 @@
 #include "Header.h"
-#include "Vector.h"
-
-using namespace std;
 
 int main() {
-	vector<string> matr = f_processing("inp.txt");
-	std::vector<vertice> vertices = get_vertices(matr);
+	vector<std::string> matr = f_processing("inp.txt");
+	vector<vertice> vertices = get_vertices(matr);
 
 	/* Zeeelensky, there is some place for your code) */
+
+
 
 	outp_path(matr, vertices);
 }
