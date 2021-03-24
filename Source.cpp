48b7c204a5c00af466450795376a0f2d21e198b5
Andrew <andriycherpak.education@gmail.com>
2021-03-24 21:53:45 +0200

    Ще кілька рандомних тестів, пофіксив виведення для шляху >36

--- Source.cpp
+++ Source.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main() {
 	int x_st, y_st, x_fin, y_fin, fin_ind, dist;
-	vector<std::string> matr = f_processing("inp.txt");
-	cout<<"Enter the coordinates of start point: "<<endl;
+	vector<std::string> matr = f_processing("inp2.txt");
+	cout<<"Enter the coordinates of the starting point: "<<endl;
 	cout<<"\tx = "; cin>>x_st; cout<<endl;
 	cout<<"\ty = "; cin>>y_st; cout<<endl;
-	cout<<"Enter the coordinates of finish point: "<<endl;
+	cout<<"Enter the coordinates of the end point: "<<endl;
 	cout<<"\tx = "; cin>>x_fin; cout<<endl;
 	cout<<"\ty = "; cin>>y_fin; cout<<endl;
 	if (matr[x_st][y_st]=='X'|| matr[x_fin][y_fin] == 'X') { cout << "The path doesn't exist!" << endl; exit(0); }
